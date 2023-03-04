7888f799fd121fc2bb6fa116adb19d1b60d749d9
Дмитрий <81642929+ise-dima@users.noreply.github.com>
2023-03-04 22:02:06 +0300

    Section 2345

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,31 @@
 function myFunction(x) {
-    x.classList.toggle("change");
-    document.getElementById("menu").classList.toggle("active");
+  x.classList.toggle("change");
+  document.getElementById("menu").classList.toggle("active");
 }
+
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+});
